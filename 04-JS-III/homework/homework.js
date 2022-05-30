8412fa0f20ad1b1489419df4f0a8c53e34db0eaf
rodrigomalegre@gmail.com <rodrigomalegre@gmail.com>
2022-05-30 12:58:20 -0300

    resuelto promedio resultadosTest

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -97,6 +97,12 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  var sumar = 0;
+  for(var i = 0 ; i < resultadosTest.length; i++){
+    sumar += resultadosTest[i];
+  }
+  return (sumar / resultadosTest.length)
+
 }
 
 
